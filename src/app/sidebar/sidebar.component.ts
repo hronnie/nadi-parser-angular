4d8ef2a2a397b3a04e3a0f3e1f1991ad47dd7845
Aron Harsfalvi <h.roni23@gmail.com>
2020-06-12 12:24:58 +0200

    Initial commit with paper dashboard template

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {UserComponent} from '../pages/user/user.component';
+import {TableComponent} from '../pages/table/table.component';
+import {TypographyComponent} from '../pages/typography/typography.component';
+
+
+export interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+    // { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
+    // { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
+    // { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
+    // { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
+    { path: '/invites',         title: 'Tanfolyam meghívók',        icon:'nc-tile-56',    class: '' },
+    { path: '/settings',          title: 'Beállítások',      icon:'nc-settings-gear-65',  class: '' },
+    { path: '/help',    title: 'Segítség',        icon:'nc-bulb-63', class: '' },
+    // { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
+];
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'sidebar-cmp',
+    templateUrl: 'sidebar.component.html',
+})
+
+export class SidebarComponent implements OnInit {
+    public menuItems: any[];
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }
+}
